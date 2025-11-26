36aa89e9d6a59212d1fda624a3af7cc1a384826d
Juan Dorado <juan.dorado07@usc.edu.co>
2025-11-26 14:00:55 -0500

    Distribucion central de l evaluationstate

--- src/pages/unit2/EvaluationStage.jsx
+++ src/pages/unit2/EvaluationStage.jsx
@@ -15,7 +15,7 @@ export default function EvaluationStage({ onComplete, unitColor,}) {
   return (
     <GameViewport showControls={true} forceFullscreen>
       <div className="stage-container">
-        <h2 className="evaluation-title">Evaluación</h2>
+        
         {/* Escena del banco UNIT 2 */}
         <GameWorldSimple onComplete={() => {}} />
 
